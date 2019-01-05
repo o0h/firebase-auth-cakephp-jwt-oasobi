2c4301edeb6a6cff4c5e01a5f13d461aa68859a2
Hideki Kinjyo <dev@o0h.in>
2019-01-05 20:27:01 +0900

    Create users table

---
+++ api/config/Migrations/20190105111925_CreateUsers.php
@@ -0,0 +1,38 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateUsers extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $this->table('users')
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 64,
+                'null' => false
+            ])
+            ->addColumn('avatar_url', 'string', [
+                'default' => null,
+                'limit' => 256,
+                'null' => false
+            ])
+            ->addColumn('created', 'timestamp', [
+                'default' => 'CURRENT_TIMESTAMP',
+                'limit' => null,
+                'null' => false
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true
+            ])
+            ->create();
+    }
+}
